fdce8a612334ed628ee039b13527331869113038
ajilk <ajilkhomov@gmail.com>
2019-10-08 16:43:45 -0400

    Update frontend to communicate with backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+// import {BrowserRouter as Router, Route} from 'react-router'
 
 class App extends Component {
+  state = {
+    message: ''
+  }
+
+  componentDidMount = () => this.fetchMessage();
+
+  fetchMessage = async () => {
+    let ID = 1
+    const response = await fetch(`api/${ID}`)
+    const message = response.json()
+    this.setState({ message: message })
+  }
+
   render() {
     return (
       <div class="container text-center p-5">
         <h1>findfreefood</h1>
         <h5 class="text-danger">[ in-progess ]</h5>
+        <h5>{this.state.message}</h5>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const PORT = 3000
 
-app.get('/api/:message', (req,res) => {
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+app.get('/api/:message', (req, res) => {
   const msg = req.params.message
   res.send(`you sent ${msg}`)
 });
