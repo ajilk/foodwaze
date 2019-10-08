58cdf2f47dcd725dd104d34015d3dd21a54f1e60
saarhaber <saarhaber@gmail.com>
2019-10-08 17:57:41 -0400

    need to link

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
 import './Landing.css';
+import { Link } from 'react-router';
 
 class Landing extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      collegeClicked: ""
-    };
+
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(event) {
-    this.setState({ collegeClicked: event.target.value })
-    console.log(this.state.collegeClicked);
-  }
+   
+    }
 
   render() {
     return (
       <table>
-        <tr>
           <td class="hunter" value="hunter" onClick={this.handleClick}><img src={require("../logos/hunter-college-logo.png")} alt="hunter" /></td>
           <td class="brook">
             <img src={require("../logos/brooklyn-college-logo.png")} alt="brooklyn" />
