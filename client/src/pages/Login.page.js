b2e6038f1a261e76295bed6eef6a355079017173
ajilk <ajilkhomov@gmail.com>
2019-11-08 20:39:54 -0500

    Setup login and signup UI

--- client/src/pages/Login.page.js
+++ client/src/pages/Login.page.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react'
 
 export default class LoginPage extends Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  signIn = () => console.log(`U: ${this.state.email} P: ${this.state.password}`);
+  onPasswordChange = (e) => this.setState({ password: e.target.value });
+  onEmailChange = (e) => this.setState({ email: e.target.value });
+
   render() {
     return (
-      <h1>LoginPage</h1>
+      <div className="row justify-content-center">
+        <div className="col-lg-4 col-md-6 col-12">
+          <div className="row">
+            <div className="col">
+              <h2>welcome back</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="email" onChange={this.onEmailChange} />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <input type="password" className="form-control" id="passwordInput" placeholder="password" onChange={this.onPasswordChange} />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <button type="submit" className="btn btn-block btn-outline-secondary" onClick={this.signIn}>sign in</button>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- client/src/pages/Signup.page.js
+++ client/src/pages/Signup.page.js
@@ -1,9 +1,54 @@
 import React, { Component } from 'react'
 
 export default class SignupPage extends Component {
+  state = {
+    email: '',
+    password: '',
+    verifyPassword: '', 
+  }
+
+  signUp = () => console.log(`U: ${this.state.email} P: ${this.state.password}`);
+  onPasswordChange = (e) => this.setState({ password: e.target.value });
+  onVerifyPasswordChange = (e) => this.setState({ verifyPassword: e.target.value });
+  onEmailChange = (e) => this.setState({ email: e.target.value });
+
   render() {
     return (
-      <h1>SignupPage</h1>
+      <div className="row justify-content-center">
+        <div className="col-lg-4 col-md-6 col-12">
+          <div className="row">
+            <div className="col">
+              <h2>hello</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="email" onChange={this.onEmailChange} />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <input type="password" className="form-control" id="passwordInput" placeholder="password" onChange={this.onPasswordChange} />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <input type="password" className="form-control" id="verifyPasswordInput" placeholder="verify password" onChange={this.onVerifyPasswordChange} />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <button type="submit" className="btn btn-block btn-outline-secondary" onClick={this.signUp}>sign up</button>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
