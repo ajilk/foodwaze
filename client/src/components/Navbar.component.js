f9306ac93705ffebb9e6f830280dd9e95feec3b8
ajilk <ajilkhomov@gmail.com>
2019-11-09 19:04:11 -0500

    Prevent input-group from taking a new line

--- client/src/components/Navbar.component.js
+++ client/src/components/Navbar.component.js
@@ -10,13 +10,15 @@ export default class NavbarComponent extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-light">
         <Link to='/' className="navbar-brand">foodwaze</Link>
-        <div className="input-group">
-          <Link to='/login' style={{ textDecoration: 'none' }} className="ml-auto input-group-append">
-            <button className="btn btn-outline-dark border-right-0 rounded-left">login</button>
-          </Link>
-          <Link to='/signup' style={{ textDecoration: 'none' }} className="input-group-append">
-            <button className="btn btn-outline-dark rounded-right">signup</button>
-          </Link>
+        <div className="col">
+          <div className="input-group">
+            <Link to='/login' style={{ textDecoration: 'none' }} className="ml-auto input-group-append">
+              <button className="btn btn-outline-dark border-right-0 rounded-left">login</button>
+            </Link>
+            <Link to='/signup' style={{ textDecoration: 'none' }} className="input-group-append">
+              <button className="btn btn-outline-dark rounded-right">signup</button>
+            </Link>
+          </div>
         </div>
       </nav>
     )
