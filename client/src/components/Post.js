db197cb9eac6c39066602111789d263944f04e72
saarhaber <saarhaber@gmail.com>
2019-11-22 14:31:06 -0500

    styilng improved!

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -3,32 +3,17 @@ import {Card} from 'react-bootstrap';
 
 
 export default class Post extends Component {
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         // location: this.props.location,
-    //         // description: this.props.description,
-    //         // college: this.props.college,
-    //         // foodtype: this.props.foodtype,
-    //         // image: this.props.image
-    //         location: 'CS Department 10th floor',
-    //         description: "A TON of plain pizzaaa until 5pm. Kosher! Bring friends!",
-    //         college: "Hunter College",
-    //         foodtype: "Pizza",
-    //         image: "https://www.kingarthurflour.com/sites/default/files/styles/featured_image_2x/public/recipe_legacy/20-3-large.jpg?itok=1v5DoLeu"
-    //     }
-    //   }
+  
   render() {
     return (
-        <div>
-    <Card style={{ width: '30rem', borderRadius: '20px'}}>
+        <div style={{ margin: "8px"}}>
+    <Card style={{ width: '15rem', height: '100%', borderRadius: '20px'}}>
     <Card.Body>
     <Card.Title> <strong>{this.props.post.foodtype} @ </strong>
-    {this.props.post.location}
+{this.props.post.location}
     </Card.Title>
-    <Card.Text>
-        {this.props.post.description}
-        <Card.Img variant="top" src={this.props.post.image} />
+    <Card.Text>{this.props.post.description}
+        <Card.Img variant="top" style={{marginTop: "10px", borderRadius: '10px'}} src={this.props.post.image} />
     </Card.Text>
     </Card.Body>
     </Card>
