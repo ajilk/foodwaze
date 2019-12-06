eb7b0c7f97d210c4171c8c2344c1ce49214cfd1f
ajilk <ajilkhomov@gmail.com>
2019-12-06 15:56:14 -0500

    Simplify navbar on mobile

--- client/src/components/Navbar.component.js
+++ client/src/components/Navbar.component.js
@@ -10,7 +10,7 @@ export default class NavbarComponent extends Component {
       <nav className="navbar navbar-expand-lg bg-dark navbar-dark border-bottom shadow mb-5" style={{ backgroundColor: "#d3d3d3" }}>
         <div className="container">
           <Link to='/'><img className="" src={brandLogo} width="50" alt="" /></Link>
-          <Link to='/' className="pl-3 navbar-brand"><h3 className="mb-0 mt-2">foodwaze</h3></Link>
+          <Link to='/' className="pl-3 navbar-brand d-none d-sm-block"><h3 className="mb-0 mt-2">foodwaze</h3></Link>
           <AuthButton />
         </div>
       </nav >
